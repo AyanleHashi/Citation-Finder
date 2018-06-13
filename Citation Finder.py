bc958e4cf8516d685eecff80eeef11784f72619a
Ayanle Hashi <ahashi2000@gmail.com>
2018-06-13 11:37:33 -0400

    Update Citation Finder.py

--- Citation Finder.py
+++ Citation Finder.py
@@ -1,11 +1,8 @@
+from codecs import decode
 import scholarly
 import subprocess
-import codecs
-import time
-start = time.time()
 
 search_query = scholarly.search_pubs_query('Maintaining and Operating a Public Information Repository')
-#search_query = scholarly.search_pubs_query('Lung nodule detection in CT images using deep convolutional neural networks')
 pub = next(search_query)
 
 IDs = []
@@ -15,8 +12,6 @@ for citation in pub.get_citedby():
     except AttributeError:
         pass
 
-print("Finished getting citations in", time.time() - start, "seconds.")
-
 text = ""
 for i in IDs:
     command = "scholar.py -C {} --citation en".format(i)
@@ -24,11 +19,9 @@ for i in IDs:
     process = subprocess.Popen(command,stdout=subprocess.PIPE,shell=True)
 
     out = process.communicate()[0].decode("utf-8").replace(r"\r","")[2:-7]
-    out = codecs.decode(out,"unicode_escape")
+    out = decode(out,"unicode_escape")
 
     text += out + "\n\n"
 
-with open("endnote.enw","w",encoding="utf-8") as f:
+with open("endnote mini.enw","w",encoding="utf-8") as f:
     f.write(text)
-
-print("Total time:", time.time() - start, "seconds.")
